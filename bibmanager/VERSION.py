f01d27c1ea421219e3585a58a62750f0df6dd222
pcubillos <patricio.cubillos@oeaw.ac.at>
2019-01-04 12:54:20 -0300

    Initial docs draft.

--- bibmanager/VERSION.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2018 Patricio Cubillos and contributors.
-# bibmanager is open-source software under the MIT license (see LICENSE).
-
-# bibmanager Version:
-BM_VER =  0  # Major version
-BM_MIN =  4  # Minor version
-BM_REV =  0  # Revision
